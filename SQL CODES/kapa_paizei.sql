df9e4fbec7295eb4228cd44904ff56624fbd9efb
Lampros Vinis <lamprosvinis10@gmail.com>
2026-06-12 20:30:31 +0300

    Update kapa_paizei.sql

--- SQL CODES/kapa_paizei.sql
+++ SQL CODES/kapa_paizei.sql
@@ -1,27 +1,9 @@
--- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: kapa
--- ------------------------------------------------------
--- Server version	8.0.46
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `paizei`
 --
 
 DROP TABLE IF EXISTS `paizei`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `paizei` (
   `ID_AGONA` int NOT NULL,
   `ID_OMADAS` int NOT NULL,
@@ -32,25 +14,13 @@ CREATE TABLE `paizei` (
   CONSTRAINT `FK_paizei_agona` FOREIGN KEY (`ID_AGONA`) REFERENCES `agonas` (`ID_AGONA`) ON DELETE CASCADE,
   CONSTRAINT `FK_paizei_omada` FOREIGN KEY (`ID_OMADAS`) REFERENCES `omada` (`ID_OMADA`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `paizei`
 --
 
 LOCK TABLES `paizei` WRITE;
-/*!40000 ALTER TABLE `paizei` DISABLE KEYS */;
+
 INSERT INTO `paizei` VALUES (1,2,0,0),(1,4,0,0),(2,2,0,0),(2,3,0,0),(4,3,2,1),(4,4,2,1),(5,2,1,0),(5,3,1,0),(6,1,1,1),(6,3,1,1),(7,1,4,2),(7,3,4,2),(8,2,3,1),(8,3,3,1),(9,3,3,1),(9,4,3,1),(10,3,2,1),(10,4,2,1),(11,2,1,2),(11,3,1,2),(12,1,0,1),(12,3,0,1),(13,1,2,3),(13,4,2,3),(14,1,4,1),(14,2,4,1),(15,1,2,1),(15,4,2,1),(16,1,0,1),(16,2,0,1),(17,1,2,1),(17,4,2,1),(18,1,1,0),(18,2,1,0),(19,1,0,2),(19,2,0,2),(20,1,4,2),(20,4,4,2),(21,2,1,1),(21,4,1,1),(22,2,2,3),(22,4,2,3),(23,2,1,2),(23,4,1,2),(24,2,1,0),(24,4,1,0);
-/*!40000 ALTER TABLE `paizei` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-05-26 16:47:38
